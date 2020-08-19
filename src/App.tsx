1fe65b4f37b68e3c7449128917026319dee76173
Mariana Pereira <marianna.22sp@gmail.com>
2020-08-19 18:29:26 -0300

    Create global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
+import GlobalStyle from './styles/global';
+
 function App() {
-  return <div className="App" />;
+  return (
+    <>
+      <GlobalStyle />
+      <div />
+    </>
+  );
 }
 
 export default App;
---
+++ src/styles/global.ts
@@ -0,0 +1,35 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    padding: 0;
+    margin: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+  html, body, #root {
+    height: 100%;
+    background: #fff;
+  }
+
+  body {
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button {
+    font: 14px 'Monteserrat', sans-serif;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  ul {
+    list-style: none;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
