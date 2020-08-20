27d8d28eb28932be2878fed40b36eff19bce870b
Mariana Pereira <marianna.22sp@gmail.com>
2020-08-20 10:51:52 -0300

    Create home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,13 +2,75 @@ import React from 'react';
 
 import Header from '../../components/Header';
 
-import { Container } from './styles';
+import deleteIcon from '../../assets/Delete-Icon.png';
+import editIcon from '../../assets/Edit-Icon.png';
+
+import { Container, Navers, Naver } from './styles';
 
 const Home: React.FC = () => {
   return (
-    <Container>
+    <>
       <Header />
-    </Container>
+      <Container>
+        <div className="header">
+          <h1>Navers</h1>
+          <button type="button">Adicionar Naver</button>
+        </div>
+        <Navers>
+          <Naver>
+            <img
+              src="https://avatars0.githubusercontent.com/u/26336279?s=460&u=5b67ae546af49ac00ca125f373a3bbaf45a5f765&v=4"
+              alt="Mariana Pereira"
+            />
+            <strong>Mariana Pereira</strong>
+            <span>Front-end Developer</span>
+            <div>
+              <img src={deleteIcon} alt="Excluir" />
+              <img src={editIcon} alt="Editar" />
+            </div>
+          </Naver>
+
+          <Naver>
+            <img
+              src="https://avatars0.githubusercontent.com/u/26336279?s=460&u=5b67ae546af49ac00ca125f373a3bbaf45a5f765&v=4"
+              alt="Mariana Pereira"
+            />
+            <strong>Mariana Pereira</strong>
+            <span>Front-end Developer</span>
+            <div>
+              <img src={deleteIcon} alt="Excluir" />
+              <img src={editIcon} alt="Editar" />
+            </div>
+          </Naver>
+
+          <Naver>
+            <img
+              src="https://avatars0.githubusercontent.com/u/26336279?s=460&u=5b67ae546af49ac00ca125f373a3bbaf45a5f765&v=4"
+              alt="Mariana Pereira"
+            />
+            <strong>Mariana Pereira</strong>
+            <span>Front-end Developer</span>
+            <div>
+              <img src={deleteIcon} alt="Excluir" />
+              <img src={editIcon} alt="Editar" />
+            </div>
+          </Naver>
+
+          <Naver>
+            <img
+              src="https://avatars0.githubusercontent.com/u/26336279?s=460&u=5b67ae546af49ac00ca125f373a3bbaf45a5f765&v=4"
+              alt="Mariana Pereira"
+            />
+            <strong>Mariana Pereira</strong>
+            <span>Front-end Developer</span>
+            <div>
+              <img src={deleteIcon} alt="Excluir" />
+              <img src={editIcon} alt="Editar" />
+            </div>
+          </Naver>
+        </Navers>
+      </Container>
+    </>
   );
 };
 
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,3 +1,79 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  width: 100%;
+  padding: 30px;
+  display: flex;
+  flex-direction: column;
+
+  .header {
+    width: 100%;
+    height: 85px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    h1 {
+      font-size: 40px;
+      line-height: 48px;
+      font-weight: 600;
+      color: #212121;
+    }
+
+    button {
+      width: 176px;
+      height: 40px;
+      background: #212121;
+      border: 0;
+      color: #ffffff;
+      font-weight: 600;
+      line-height: 24px;
+    }
+  }
+`;
+
+export const Navers = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+`;
+
+export const Naver = styled.div`
+  width: 280px;
+  height: 376px;
+  display: flex;
+  flex-direction: column;
+
+  img {
+    width: 280px;
+    height: 280px;
+  }
+
+  strong {
+    font-size: 16px;
+    line-height: 18px;
+    font-weight: 600;
+    color: #212121;
+    margin-top: 10px;
+  }
+
+  span {
+    font-size: 16px;
+    line-height: 24px;
+    color: #212121;
+    margin-top: 6px;
+  }
+
+  div {
+    display: flex;
+    margin-top: 10px;
+
+    img {
+      width: 24px;
+      height: 24px;
+
+      & + img {
+        margin-left: 10px;
+      }
+    }
+  }
+`;
